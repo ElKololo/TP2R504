fb73cceacd88f93b76f703d2b1b2604f23779c8e
ElKololo <a.houdhayf@gmail.com>
2025-10-19 18:22:54 +0200

    test affiche n1 n2.... augmentation de la range

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -3,3 +3,8 @@ from fizzbuzz import affiche
 def test_affiche_with_n_15():
     expected = "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee"
     assert affiche(15) == expected
+def test_affiche_range_5_10():
+    assert affiche(5, 10) == "BuzzFizz78FizzBuzz"
+
+def test_affiche_range_10_16():
+    assert affiche(10, 16) == "Buzz11Fizz1314FrisBee16"
