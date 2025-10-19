3b1c0ead706abcb568bb96a074ea29b78d7aa298
ElKololo <a.houdhayf@gmail.com>
2025-10-19 18:25:04 +0200

    fonction affiche par défaut 1 à 100

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,7 +9,7 @@ def _fizzbuzz_value(i):
 
 def affiche(n1=None, n2=None):
     if n1 is None and n2 is None:
-        start, end = 1, 15  # pour l’instant 1..15 juste pour passer le 1er test TDD
+        start, end = 1, 100  
     elif n2 is None:
         start, end = 1, n1
     else:
