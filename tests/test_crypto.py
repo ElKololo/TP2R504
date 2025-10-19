59e759b11432b88c0ab709f05732e93b49b8f7b6
ElKololo <a.houdhayf@gmail.com>
2025-10-19 23:19:36 +0200

    Ajout test decrypt() inverse de crypt()

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -4,3 +4,7 @@ def test_crypt_pas_1():
     assert crypt("a") == "b1"
 def test_crypt_pas_3():
     assert crypt("az", 3)[0] == "d"
+def test_decrypt_inverse_crypt():
+    message = "Hello!"
+    chiffre = crypt(message, 4)  # on chiffre
+    assert decrypt(chiffre) == message  # on déchiffre
