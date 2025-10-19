36aba6a73a09582f273f4f42af4d0a64aea0543a
ElKololo <a.houdhayf@gmail.com>
2025-10-19 23:21:05 +0200

    Ajout de la fonction decrypt()

--- crypto.py
+++ crypto.py
@@ -14,5 +14,14 @@ def crypt(message, pas=1):
     return resultat + str(pas)
 
 def decrypt(message):
-    # Décryptage ajouté plus tard
-    pass
+    pas = int(message[-1])  # dernier caractère = pas utilisé
+    message = message[:-1]  # on enlève le pas à la fin
+    resultat = ""
+    for c in message:
+        if c in CARACTERES:
+            index = CARACTERES.index(c)
+            resultat += CARACTERES[(index - pas) % len(CARACTERES)]
+        else:
+            resultat += c
+    return resultat
+
